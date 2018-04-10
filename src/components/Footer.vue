3655a8b43be66471999d5f0d85cbeee2149640b2
Amir Shahbandegan <am.shahbandegan@gmail.com>
2018-04-10 17:31:02 +0430

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,16 +5,41 @@
       <div class="row margin-top-60 margin-bottom-40 size-13">
 
         <!-- col #1 -->
-        <div class="col-md-3 col-sm-3">
-
+        <div class="col-md-4">
           <!-- Footer Logo -->
           <router-link class="logo " to="/">
-            <img src="http://new.asansport.com/img/logo.png" alt=""/>
+            <img class="img-responsive" src="http://new.asansport.com/img/logo.png" alt=""/>
           </router-link>
-
-
           <h2></h2>
 
+        </div>
+        <div class="col-md-4">
+          <h4>آسان اسپرت</h4>
+          <ul class="list-unstyled footer-list half-paddings noborder">
+            <li><router-link class="block" to="/about"><i class="fa fa-angle-left"> </i> درباره ما</router-link></li>
+            <li><router-link class="block" to="/how-it-works"><i class="fa fa-angle-left"> </i> چگونه کار می کند؟</router-link></li>
+            <li><router-link class="block" to="lottery"><i class="fa fa-angle-left"> </i> قرعه کشی</router-link></li>
+            <li><router-link class="block" to="/terms"><i class="fa fa-angle-left"> </i> قوانین و مقررات </router-link></li>
+            <li><router-link class="block" to="/faq"><i class="fa fa-angle-left"> </i> سوالات متداول </router-link></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <div style="background: white; width: 100%; border-radius: 10px; padding: 8px;" class="row">
+            <div class="col-md-6 text-center"><img src="../assets/images/enamad.png" /></div>
+            <div class="col-md-6 text-center"><img src="../assets/images/samandehi.png" /></div>
+          </div>
+          <!--<img id='fukzjxlzoeuknbqejzpe' style='cursor:pointer' onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=61827&p=gvkarfthmcsiuiwkjyoe", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")' alt='logo-samandehi' src='https://logo.samandehi.ir/logo.aspx?id=61827&p=wlbqnbpdaqgwodrfyndt'/>-->
+          <!--<img id='drftsguibrgwlbrhrgvl' style='cursor:pointer' onclick='window.open("https://trustseal.enamad.ir/Verify.aspx?id=10675&p=nbpddrfshwmbqgwlyncr", "Popup","toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30")' alt='' src='https://trustseal.enamad.ir/logo.aspx?id=10675&p=lznbgthvkzoepeukfujy'/>-->
+        </div>
+        <!-- /col #1 -->
+
+      </div>
+
+    </div>
+
+    <div class="copyright">
+      <div class="container">
+        <div class="pull-right nomargin mobile-block">
           <!-- Social Icons -->
           <div class="clearfix pull-right">
 
@@ -50,51 +75,9 @@
 
           </div>
           <!-- /Social Icons -->
-
         </div>
-        <!-- /col #1 -->
 
-        <!-- col #2 -->
-        <div class="col-md-7 col-sm-7 pull-right">
-
-          <div class="row">
-
-            <div class="col-md-3 hidden-sm hidden-xs">
-              <h4 class="letter-spacing-1">آسان اسپرت</h4>
-              <ul class="list-unstyled footer-list half-paddings noborder">
-                <li><router-link class="block" to="/about"><i class="fa fa-angle-right"></i> درباره ما</router-link></li>
-                <li><router-link class="block" to="/how-it-works"><i class="fa fa-angle-right"></i> چگونه کار می کند؟</router-link></li>
-                <li><router-link class="block" to="lottery"><i class="fa fa-angle-right"></i> قرعه کشی</router-link>
-                </li>
-                <li><router-link class="block" to="/terms"><i class="fa fa-angle-right"></i> قوانین و مقررات </router-link></li>
-                <li><router-link class="block" to="/faq"><i class="fa fa-angle-right"></i> سوالات متداول </router-link></li>
-              </ul>
-            </div>
-
-            <div class="col-md-4">
-                <img id="fukzjxlzoeuknbqejzpe" style="cursor:pointer"  alt="logo-samandehi" src="https://logo.samandehi.ir/logo.aspx?id=61827&amp;p=wlbqnbpdaqgwodrfyndt">
-                <img id="drftsguibrgwlbrhrgvl" style="cursor:pointer" alt="" src="https://trustseal.enamad.ir/logo.aspx?id=10675&amp;p=lznbgthvkzoepeukfujy">
-
-            </div>
-
-          </div>
-
-        </div>
-        <!-- /col #2 -->
-
-      </div>
-
-    </div>
-
-    <div class="copyright">
-      <div class="container">
-        <ul class="pull-right nomargin list-inline mobile-block">
-          <li><a href="#">Terms &amp; Conditions</a></li>
-          <li>&bull;</li>
-          <li><a href="#">Privacy</a></li>
-        </ul>
-
-        &copy; All Rights Reserved, Company LTD
+        تمامی حقوق متعلق به آسان اسپرت می باشد.
       </div>
     </div>
 
