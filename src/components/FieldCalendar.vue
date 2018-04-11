3c3d8e74f23104a3ec870b80ea8f925e68e6ab1d
s3000 <s3000@yahoo.com>
2018-04-11 11:24:27 +0430

    Added Comments in Fields

--- src/components/FieldCalendar.vue
+++ src/components/FieldCalendar.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="calendar"></div>
+  <div>
+    <div id="calendar">
+    </div>
+  </div>
+
 </template>
 
-<script2 src="/static/assets/js/calendar.js" type="text/javascript"></script2>
 
 <script>
   export default {
@@ -11,45 +14,44 @@
         name: 'FieldCalendar'
       }
     },
-      mounted() {
-          var calendar = $('#calendar').fullCalendar({
-              header: {
-                  left: 'next,prev today',
-                  right: 'title'
-//                    right: 'month,agendaWeek,agendaDay'
-              },
-              buttonIcons: {
-                  prev: 'right-single-arrow',
-                  next: 'left-single-arrow',
-                  prevYear: 'left-double-arrow',
-                  nextYear: 'right-double-arrow'
-              },
-              isJalaali: true,
-              isRTL: true,
-              navlinks: false,
-              lang: 'fa',
-              eventLimit: false, // allow "more" link when too many events
-              defaultView: 'agendaWeek',
-              height: 550,
-              allDaySlot: false,
-              eventDurationEditable: false,
-              eventOverlap: false,
-              slotDuration: '00:30:00',
-              slotLabelFormat: 'hh:mm',
-              loading: function (bool) {
-                  $('#loading').toggle(bool);
-              },
-              editable: false,
+    mounted() {
+      var calendar = $('#calendar').fullCalendar({
+        header: {
+          left: 'next,prev today',
+          right: 'title'
+        },
+        buttonIcons: {
+          prev: 'right-single-arrow',
+          next: 'left-single-arrow',
+          prevYear: 'left-double-arrow',
+          nextYear: 'right-double-arrow'
+        },
+        isJalaali: true,
+        isRTL: true,
+        navlinks: false,
+        lang: 'fa',
+        eventLimit: false, // allow "more" link when too many events
+        defaultView: 'agendaWeek',
+        height: 550,
+        allDaySlot: false,
+        eventDurationEditable: false,
+        eventOverlap: false,
+        slotDuration: '00:30:00',
+        slotLabelFormat: 'hh:mm',
+        loading: function (bool) {
+          $('#loading').toggle(bool);
+        },
+        editable: false,
 //            eventDrop: eventMoved,
-              events: {
-                  url: `http://api.shahbandegan.ir/v1/fields/y6NZ6Q/schedule`,
-                  method: "post",
-                  error: function () {
-                      $('#loadingError').show();
-                  }
-              }
-          });
-      }
+        events: {
+          url: `http://api.shahbandegan.ir/v1/fields/y6NZ6Q/schedule`,
+          method: "post",
+          error: function () {
+            $('#loadingError').show();
+          }
+        }
+      });
+    }
   }
 </script>
 
